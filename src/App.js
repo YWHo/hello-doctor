e9de8497a326715cc34ee248e687289f339d501d
Brian Ho <YWHo@users.noreply.github.com>
2019-06-14 23:23:02 +1200

    initial code to test fetching data from the server

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import { fetchSchedule } from './apiRequester';
 
 function App() {
+  const [schedules, setSchedules] = useState([]);
+
+  useEffect(() => {
+    fetchSchedule('2019-06-13')
+    .then(json => {
+      console.log('returned:\n', json);
+      setSchedules(json);
+    });
+  }, []);
+
+  const nameList = Array.from(schedules).map((item, idx) => {
+    return (
+      <div key={idx}>
+        <div>Name: {item.Name}</div>
+        <div>Title: {item.Title}</div>
+        <br />
+      </div>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      { nameList }
     </div>
-  );
+  ) 
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/apiRequester.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+
+export function fetchSchedule(date) {
+  const fullUrl = `https://frontendchallenge2019.azurewebsites.net/api/Schedule/${date}`;
+  return remoteGet(fullUrl);
+}
+
+export function fetchProvider(id) {
+  const fullUrl = `https://frontendchallenge2019.azurewebsites.net/api/Provider/${id}`;
+  return remoteGet(fullUrl);
+}
+
+export function fetchPicture(id) {
+  const fullUrl = `https://frontendchallenge2019.azurewebsites.net/api/Picture/${id}`;
+  return remoteGet(fullUrl);
+}
+
+function remoteGet(url) {
+  return new Promise((resolve, reject) => {
+    axios.get(url)
+      .then(res => resolve(JSON.parse(res.data)))
+      .catch(err => reject(err))
+  });
+}
