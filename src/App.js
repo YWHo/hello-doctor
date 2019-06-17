e7d69d72a68e72785f48a9ed642705c08a164995
Brian Ho <YWHo@users.noreply.github.com>
2019-06-17 18:27:52 +1200

    add ErrorBoundary to catch Javascript error in React components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
 // import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css'
+import ErrorBoundary from './ErrorBoundary'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import AppointmentPage from './components/AppointmentPage'
 import RegisterPage from './components/RegisterPage'
 import actions from './state/actions'
 
-export function App(props) {
+export function App (props) {
   useEffect(() => {
-    const { dToFetchSchedules } = props 
-    dToFetchSchedules('2019-06-13');
-  }, [props]);
+    const { dToFetchSchedules } = props
+    dToFetchSchedules('2019-06-13')
+  }, [props])
 
   return (
-    <Router>
-      <div>
-        <Route exact path="/" component={() => <AppointmentPage />} />
-        <Route path="/register" component={() => <RegisterPage />} />
-      </div>
-    </Router>
-  );
+    <ErrorBoundary>
+      <Router>
+        <div>
+          <Route exact path='/' component={() => <AppointmentPage />} />
+          <Route path='/register' component={() => <RegisterPage />} />
+        </div>
+      </Router>
+    </ErrorBoundary>
+  )
 }
 
-const mapStateToProps = (state) => {
-  return { }
+const mapStateToProps = state => {
+  return {}
 }
 
 function mapDispatchToProps (dispatch) {
   return {
-    dToFetchSchedules: (dateStr) => dispatch(actions.toFetchSchedules(dateStr)),
+    dToFetchSchedules: dateStr => dispatch(actions.toFetchSchedules(dateStr))
   }
 }
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
