2ea38101a6e87e668f88f65697f800410883d93e
Brian Ho <YWHo@users.noreply.github.com>
2019-06-20 23:32:23 +1200

    enable TimeSlots to show basic content

--- src/components/TimeSlots/TimeSlots.js
+++ src/components/TimeSlots/TimeSlots.js
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 import dayjs from 'dayjs';
 
 const ContainerOuter = styled.div`
-  background-color: #fcfcfc;
   margin-top: 16px;
   height: 37.95px;
   width: 100%;
@@ -12,12 +11,38 @@ const ContainerOuter = styled.div`
   white-space: nowrap;
 `;
 
+const ContainerNoAppointment = styled.div`
+  margin-top: 25.19px;
+  height: 38.37px;
+  padding-right: 19.88px;
+  width: 100%;
+`;
+
+const NoAppointment = styled.div`
+  background-color: #f7f7f7;
+  font-size: 16px;
+  line-height: 38.37px;
+  height: 38.37px;
+  width: 100%;
+  text-align: center;
+  vertical-align: middle;
+`;
+
 export function TimeSlots(props) {
-  const { availableSlots } = props;
+  const { availableSlots = {} } = props;
   const slots = Object.keys(availableSlots).map(key =>
     dayjs(availableSlots[key]).format('HH:mm')
   );
-  return <ContainerOuter>{slots.join(' | ')}</ContainerOuter>;
+
+  if (slots.length === 0) {
+    return (
+      <ContainerNoAppointment>
+        <NoAppointment>No appointment available for this date</NoAppointment>
+      </ContainerNoAppointment>
+    );
+  } else {
+    return <ContainerOuter>{slots.join(' | ')}</ContainerOuter>;
+  }
 }
 
 const mapStateToProps = state => {
